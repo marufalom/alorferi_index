bccc03952f8c636173aa13e5b9b77a8aea08129d
sabuj <sabujcse16@gmail.com>
2015-05-31 00:07:43 +0600

    about.php

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <p>
 <?php
-$menu = array("হোম"=>"home","সম্পর্কে"=>"home","যোগাযোগ"=>"home" );
+$menu = array("à¦¹à§‹à¦®"=>"home","à¦¸à¦®à§�à¦ªà¦°à§�à¦•à§‡"=>"about.php","à¦¯à§‹à¦—à¦¾à¦¯à§‹à¦—"=>"home" );
 
 foreach ($menu as  $k =>  $v  ){
 	print "<a href='$v'>$k</a> | ";
