f5e2c2fc72155cb39a4d86d93f9bca500a91d853
User <User@SABUJ>
2015-06-12 01:28:42 +0600

    add comment item in contact.php

--- contact.php
+++ contact.php
@@ -5,13 +5,18 @@ include_once 'header.php';
 
 <html>
 <head>
+<meta charset = "UTF-8">
 <link href="style.css" rel="stylesheet" type="text/css">
-<meta charset = "UTF-8";
 </head>
 <body>
 <h3>লাইব্রেরী সংক্রান্ত যে কোন প্রয়োজনে আপনি আমাদের  মেইল করতে পারেন নিচের  ঠিকানায়</h3>
 <h2><a href="alorferi@gmail.com"><em>alorferi@gmail.com</em></a></h2>
 <h3> এছাড়া আপনি সরাসরি মোবাইলেও যোগাযোগ করতে পারেন এই নম্বরে- <em>০১৬১৬৯৮৩৪৭৩ </em></h3>
+<h4><font color="maroon">আলোর ফেরী সম্পর্কে আপনি যে কোন মন্তব্য করুন নিচে-</h4>
+
+আপনার নাম : <input type = "text",name= "name" value = ""><br>
+<textarea rows="6" cols="50"></textarea><br>
+<button type="button" onclick="alert('Submitted')">Submit</button>
 </body>
 </html>
 
