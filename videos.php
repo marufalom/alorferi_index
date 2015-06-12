c2295b0f6faf4ea2211bf752693824051bf460cf
User <User@SABUJ>
2015-06-12 22:15:27 +0600

    update youtube setting in videos.php

---
+++ videos.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'header.php'; 
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset = "UTF-8">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/iccenQyqP1o?rel=0" frameborder="0" allowfullscreen></iframe>
+
+</body>
+</html>
+
+<?php
+include_once 'footer.php'; 
+?>
